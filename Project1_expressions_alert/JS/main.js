ea99de8846f5c91b74178291169afa44465eaf78
lilyally99 <omoruyiallen@yahoo.com>
2026-03-17 18:01:11 -0400

    Add Project1 expressions alert

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,16 @@
+
+//This statement below is the first part of the sentence
+var Sent1="This is the beginning of the string";
+
+//This one below is the second part of the sentence
+
+var Sent2=" and this is the end of the string";
+
+var totalsentence= Sent1 + Sent2;
+//The above is done to concatenate the sentences
+
+window.alert(totalsentence);
+//The above displays the sentence like a popup on the page
+
+document.write(totalsentence);
+//The above just displays the sentence like a normal sentence on the webpage
