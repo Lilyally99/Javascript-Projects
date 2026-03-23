5e5538c286f088d720c9c04f69e2cb13a1edf9ac
lilyally99 <omoruyiallen@yahoo.com>
2026-03-23 15:10:14 -0400

    Basic Javascript Projects

---
+++ Basic Javascript Projects/Project8_string_methods/main.js
@@ -0,0 +1,21 @@
+// Using concat() to join multiple strings
+
+let firstName = "Allen";
+let lastName = "Omoruyi";
+let message = "Welcome to my website, ";
+
+// Combine the strings using concat()
+let fullMessage = message.concat(firstName, " ", lastName, "!");
+
+// Display the result in the console
+console.log(fullMessage);
+// Using slice() to display part of a string
+
+let sentence = "Welcome to my JavaScript assignment!";
+
+// slice(start, end) → extracts part of the string
+let section = sentence.slice(11, 25);
+
+// Display in console
+console.log(section);
+
